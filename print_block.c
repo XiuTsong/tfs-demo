b7344166a24f2d08d4f3f330b968f5c2a5076635
wyc <1042718192@qq.com>
2024-04-09 17:02:49 +0800

    feature: support multiple layer printing

--- print_block.c
+++ print_block.c
@@ -1,11 +1,15 @@
 #include "print_block.h"
 #include "easy_block.h"
 #include <stdio.h>
-#include <string.h>
 
 print_system_t print_core;
-#define DEFAULT_PRINT_SIZE 3
-#define DEFAULT_PRINT_INTERVAL 3
+
+#define DEFAULT_PRINT_SIZE 4
+#define DEFAULT_PRINT_INTERVAL 4
+#define DEFAULT_BLOCK_PER_LAYER 8
+#define DEFAULT_LAYER_INTERVAL 2
+
+const char *default_symbol = "@";
 
 static void init_print_system(uint32_t start_block_id, uint32_t block_num)
 {
@@ -54,10 +58,12 @@ static void __print_top_or_bottom(uint32_t state)
     uint32_t j;
     for (j = 0; j < print_core.print_size; j++) {
         if (j == print_core.print_size - 1) {
-            __print_colored_string(state, "*");
+            __print_colored_string(state, default_symbol);
         }
-        else
-            __print_colored_string(state, "* ");
+        else {
+            __print_colored_string(state, default_symbol);
+            printf(" ");
+        }
     }
 }
 
@@ -65,12 +71,16 @@ static void __print_middle(uint32_t state)
 {
     uint32_t j;
     for (j = 0; j < print_core.print_size; j++) {
-        if (j == 0)
-            __print_colored_string(state, "* ");
+        if (j == 0) {
+            __print_colored_string(state, default_symbol);
+            printf(" ");
+        }
         else if (j == print_core.print_size - 1)
-            __print_colored_string(state, "*");
-        else if (is_block_alloc(state))
-            __print_colored_string(state, "* ");
+            __print_colored_string(state, default_symbol);
+        else if (is_block_alloc(state)) {
+            __print_colored_string(state, default_symbol);
+            printf(" ");
+        }
         else
             printf("  ");
     }
@@ -84,10 +94,10 @@ static void __print_interval()
     }
 }
 
-static void print_block_top_or_bottom()
+static void print_block_top_or_bottom(uint32_t start, uint32_t num)
 {
 	uint32_t i;
-	for (i = 0; i < print_core.print_num; i++) {
+	for (i = start; i < start + num; i++) {
         __print_top_or_bottom(print_core.print_state[i]);
         if (i != print_core.print_num - 1)
             __print_interval();
@@ -95,10 +105,10 @@ static void print_block_top_or_bottom()
 	printf("\n");
 }
 
-static void print_block_middle()
+static void print_block_middle(uint32_t start, uint32_t num)
 {
 	uint32_t i;
-	for (i = 0; i < print_core.print_num; i++) {
+	for (i = start; i < start + num; i++) {
         __print_middle(print_core.print_state[i]);
         if (i != print_core.print_num - 1)
             __print_interval();
@@ -106,15 +116,39 @@ static void print_block_middle()
 	printf("\n");
 }
 
-static void __print_blocks()
+static void __print_one_layer(uint32_t start, uint32_t num)
 {
     uint32_t i;
     for (i = 0; i < print_core.print_size; i++) {
         if (i == 0 || i == print_core.print_size - 1) {
-            print_block_top_or_bottom();
+            print_block_top_or_bottom(start, num);
         } else {
-            print_block_middle();
+            print_block_middle(start, num);
         }
+    }
+}
+
+static void __print_blocks()
+{
+    uint32_t i;
+    uint32_t block_per_layer = DEFAULT_BLOCK_PER_LAYER;
+    uint32_t layer_interval = DEFAULT_LAYER_INTERVAL;
+    uint32_t layer_num = print_core.print_num / block_per_layer;
+    uint32_t last_line_block_num = print_core.print_num % block_per_layer;
+
+    printf("\n");
+    for (i = 0; i < layer_num; i++) {
+        __print_one_layer(i * block_per_layer, block_per_layer);
+        if (i != layer_num - 1) {
+            for (uint32_t j = 0; j < layer_interval; j++) {
+                printf("\n");
+            }
+        }
+    }
+    printf("\n");
+
+    if (last_line_block_num != 0) {
+        __print_one_layer(layer_num * block_per_layer, last_line_block_num);
     }
 }
 
